a6aaf1678c27951e09d1267bc9f464f43989aae9
Isa <isa0021@mail.ru>
2022-09-21 21:20:42 +0300

    Сверстал ui компонент переключения(Tabs)

--- client/src/pages/services.tsx
+++ client/src/pages/services.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Search from '../components/ui/Search';
+import BasicTabs from '../components/ui/Tabs';
 
 const Services = () => {
   return (
     <>
       <Search />
+      <BasicTabs />
       <p>Lorem ipsum dolor sit amet.</p>
       <p>Lorem ipsum dolor sit amet.</p>
     </>
