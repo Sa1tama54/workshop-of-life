69ea27b91a65ba3cbaaa98d4e1f7b3a4837f1c9d
Isa <isa0021@mail.ru>
2022-11-02 23:47:58 +0300

    Полностью докончил верстку страницы services

--- client/src/pages/services.tsx
+++ client/src/pages/services.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import InformBanner from '../components/InformBanner/InformBanner';
 import ServicesNavbar from '../components/ServicesNavbar/ServicesNavbar';
 import Paginations from '../components/ui/Pagination';
@@ -8,19 +7,6 @@ import BasicTabs from '../components/ui/Tabs';
 import MainLayout from '../layouts/MainLayout';
 import ServicesCard from '../components/ServicesCard/ServicesCard';
 import styles from '../styles/Pages.module.scss';
-
-const RightBlock = styled.div`
-  width: 823px;
-  height: 500px;
-  margin-left: 97px;
-`;
-
-const SecondBlock = styled.div`
-  width: 90%;
-  margin-left: 10%;
-`;
-
-const TabsBlock = styled.div``;
 
 const Services: React.FC = () => {
   return (
