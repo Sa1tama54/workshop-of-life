e95582aea9289617f2dfa3e5991f27d4369ee698
AlievIB <020alievib@gmail.com>
2022-11-09 23:13:22 +0300

    not done yet

--- client/src/pages/reviews/index.tsx
+++ client/src/pages/reviews/index.tsx
@@ -1,12 +1,16 @@
 import ReviewsScore from 'components/Reviews/ReviewsScore';
 import MainLayout from 'layouts/MainLayout';
 import Heading from 'components/ui/Heading';
+import ReviewsWithProgressBar from 'components/Reviews/ReviewsProgressBar';
 
 const Reviews = () => {
   return (
     <MainLayout hideTabs={true}>
       <Heading>Отзывы</Heading>
-      <ReviewsScore />
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <ReviewsScore />
+        <ReviewsWithProgressBar />
+      </div>
     </MainLayout>
   );
 };
