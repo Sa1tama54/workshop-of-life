a111ca7335612947068ea086abfb0ecfdc7879e2
Isa <isa0021@mail.ru>
2022-11-14 15:12:56 +0300

    Сверстал модальное окно

--- client/src/components/ui/Modal/index.tsx
+++ client/src/components/ui/Modal/index.tsx
@@ -4,8 +4,8 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import styles from './Modal.module.scss';
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -20,29 +20,47 @@ export default function FormDialog() {
 
   return (
     <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
+      <Button variant="outlined" onClick={handleClickOpen} className={styles.buttonModal}>
+        Оставить заявку
       </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+      <Dialog open={open} onClose={handleClose} className={styles.from}>
+        <p className={styles.closeWindow} onClick={handleClose}>
+          ❌
+        </p>
+        <DialogTitle className={styles.application}>Заявка</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
+          <TextField
+            className={styles.formInput}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Имя и фамилия"
+            type="email"
+            fullWidth
+          />
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
+            label="+7 (800)-555-35-35"
             type="email"
             fullWidth
-            variant="standard"
           />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Опишите вашу проблему"
+            type="text"
+            fullWidth
+            // variant="standard"
+          />
+          <DialogTitle className={styles.servicesName}>
+            Услуга: Название выбранной услуги
+          </DialogTitle>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button className={styles.btn}> Отправить</Button>
         </DialogActions>
       </Dialog>
     </div>
