6b9762118096ed3b150ea7e9e25d353db879dfc4
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-11-28 14:01:41 +0300

    Update products.controller.js

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -13,7 +13,7 @@ const create = async (req, res) => {
 
     res.json(product);
   } catch (error) {
-    res.status(500).json({ message: 'Не удалось добавить товар' + error });
+    res.status(500).json({ message: 'Не удалось добавить товар' });
   }
 };
 
