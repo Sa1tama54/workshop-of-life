6170d57983ac54ac910ed75117f74b2cadfb02e6
AlievIB <020alievib@gmail.com>
2022-12-09 23:14:47 +0300

    Добавил экспорт компонента PicsReview для pages
    Добавил экспорт компонента ClientReview для pages

--- client/src/components/Reviews/index.tsx
+++ client/src/components/Reviews/index.tsx
@@ -2,6 +2,6 @@ export { default as AverageRating } from './AverageRating';
 
 export { default as ProgressBarReview } from './ProgressBarReview';
 
-export { default as PicsReview } from './PicsReview';
+export { default as PicsReview } from './PicsReviews';
 
-export { default as ClientReview } from './ClientReview';
+export { default as ClientReview } from './ClientReviews';
